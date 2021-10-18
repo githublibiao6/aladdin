9259bd7e4963c45174fffde5bd879c9e5eb5949e
cles <1446977718@qq.com>
2021-10-18 23:24:08 +0800

    启动错误修复

--- extract-dao/src/main/java/com/aladdin/mis/dao/manager/UserDao.java
+++ extract-dao/src/main/java/com/aladdin/mis/dao/manager/UserDao.java
@@ -2,7 +2,6 @@ package com.aladdin.mis.dao.manager;
 
 import com.aladdin.mis.manager.bean.User;
 import com.aladdin.mis.manager.qo.UserQo;
-import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -25,7 +24,6 @@ public interface UserDao {
      * @return: java.util.List<com.apps.omnipotent.manager.bean.User>
      * @version: 1.0.0
      */
-    @Select( "select * from be_user m where m.sys005='1' and name like '%${name}%'")
     List<User> listUser(UserQo qo);
 
 }
