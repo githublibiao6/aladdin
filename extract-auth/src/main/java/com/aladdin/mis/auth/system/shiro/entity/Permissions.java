fa686a3e1be9f2ec7334dd30f45d5246ee062205
cles <1446977718@qq.com>
2021-12-01 23:46:35 +0800

    sso 校验中心

---
+++ extract-auth/src/main/java/com/aladdin/mis/auth/system/shiro/entity/Permissions.java
@@ -0,0 +1,23 @@
+package com.aladdin.mis.auth.system.shiro.entity;
+/**
+ * Created by cles on 2020/4/17 23:56
+ */
+
+import lombok.Data;
+
+/**
+ * @description: 权限信息
+ * @Auther cles
+ * @Date 2020/4/17 23:56
+ */
+@Data
+public class Permissions {
+
+    public Permissions(String id,String permissionsName){
+        this.id = id;
+        this.permissionsName = permissionsName;
+    }
+    private String id;
+    private String permissionsName;
+
+}
