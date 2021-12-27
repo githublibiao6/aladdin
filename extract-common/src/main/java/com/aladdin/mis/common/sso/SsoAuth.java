a29c69a509c021305a1300b555935ab458a1fa3e
cles <1446977718@qq.com>
2021-12-28 00:18:10 +0800

    sso的构思

---
+++ extract-common/src/main/java/com/aladdin/mis/common/sso/SsoAuth.java
@@ -0,0 +1,45 @@
+package com.aladdin.mis.common.sso;
+/*
+ *  Created by cles on 2021/12/28 0:10
+ */
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author cles
+ * @description: sso验证
+ * @Date 2021/12/28 0:10
+ * @version: 1.0.0
+ */
+@Configuration
+@ConfigurationProperties(prefix = "global.sso")
+public class SsoAuth {
+
+    /**
+     * 是否启用sso
+     */
+    private boolean enable;
+
+    /**
+     * sso路径
+     */
+    private String  url;
+
+    public void setEnable(boolean enable) {
+        this.enable = enable;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    /**
+     * 检验是否登录
+     * @param token
+     */
+    public static void verifyLogin(String token){
+
+
+    }
+}
