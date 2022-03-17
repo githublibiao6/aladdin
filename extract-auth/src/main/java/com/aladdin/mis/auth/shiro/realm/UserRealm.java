ffb290a9c93bfb6392ecce1a8d5a37244addf195
cles <1446977718@qq.com>
2022-03-17 22:47:16 +0800

    一个控制权限的连接

--- extract-auth/src/main/java/com/aladdin/mis/auth/shiro/realm/UserRealm.java
+++ extract-auth/src/main/java/com/aladdin/mis/auth/shiro/realm/UserRealm.java
@@ -113,6 +113,7 @@ public class UserRealm extends AuthorizingRealm {
     }
 
     /**
+     * http://refined-x.com/2017/08/29/%E5%9F%BA%E4%BA%8EVue%E5%AE%9E%E7%8E%B0%E5%90%8E%E5%8F%B0%E7%B3%BB%E7%BB%9F%E6%9D%83%E9%99%90%E6%8E%A7%E5%88%B6/
     * @Description: 定义如何获取用户信息的业务逻辑，给shiro做登录
     * @Param: [authenticationToken]
     * @return: org.apache.shiro.authc.AuthenticationInfo
