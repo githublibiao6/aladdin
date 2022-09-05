876933661620b5ff46a49bbe696dea10db67a430
cles <1446977718@qq.com>
2022-09-05 23:06:31 +0800

    项目计划人员

--- extract-omnipotent/src/main/java/com/aladdin/mis/omnipotent/engineering/controller/ProjectPlanUserController.java
+++ extract-omnipotent/src/main/java/com/aladdin/mis/omnipotent/engineering/controller/ProjectPlanUserController.java
@@ -1,24 +1,18 @@
 package com.aladdin.mis.omnipotent.engineering.controller;
 
+import com.aladdin.mis.common.system.controller.GlobalController;
+import com.aladdin.mis.common.system.entity.Result;
+import com.aladdin.mis.common.system.service.GlobalService;
 import com.aladdin.mis.engineering.entity.ProjectPlanUser;
 import com.aladdin.mis.engineering.service.ProjectPlanUserService;
-import com.aladdin.mis.common.system.controller.GlobalController;
-import com.aladdin.mis.engineering.qo.ProjectPlanUserQo;
-import com.aladdin.mis.engineering.vo.ProjectPlanUserVo;
-import com.aladdin.mis.common.annotation.WebLog;
-import com.aladdin.mis.common.system.entity.Result;
-import com.github.pagehelper.PageInfo;
-import com.aladdin.mis.common.system.service.GlobalService;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
 /**
- * 项目计划参与人员 ProjectPlanUserService--- 
+ * 项目计划参与人员 ProjectPlanUserService---
  * @author cles
  * @date 2022-07-05 21:57:37
 */
@@ -35,4 +29,32 @@ public class ProjectPlanUserController  extends GlobalController<ProjectPlanUser
         return projectPlanUserService ;
     }
 
+
+    /**
+     * 保存数据
+     */
+    @RequestMapping("save")
+    @ResponseBody
+    public Result save(@RequestBody ProjectPlanUser entity) {
+        boolean flag = projectPlanUserService.save(entity);
+        if(flag){
+            return Result.success();
+        }else {
+            return Result.error();
+        }
+    }
+
+    /**
+     * 删除项目计划人员
+     */
+    @RequestMapping("deleteUser")
+    @ResponseBody
+    public Result deleteUser(@RequestBody ProjectPlanUser entity) {
+        boolean flag = projectPlanUserService.deleteUser(entity);
+        if(flag){
+            return Result.success();
+        }else {
+            return Result.error();
+        }
+    }
 }
