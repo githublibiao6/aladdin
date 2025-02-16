d441e75c0846ed253e2868c59c8f40c99bf7fa61
cles <1446977718@qq.com>
2025-02-16 23:46:55 +0800

    socketio 和 websicket的抉择

--- extract-chat/src/main/java/com/aladdin/mis/socket/config/WebSocketConfig.java
+++ extract-chat/src/main/java/com/aladdin/mis/socket/config/WebSocketConfig.java
@@ -1,6 +1,8 @@
 package com.aladdin.mis.socket.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 /**
  * 首先注入一个ServerEndpointExporterBean,该Bean会自动注册使用@ServerEndpoint注解申明的websocket endpoint
@@ -14,8 +16,8 @@ public class WebSocketConfig {
      * 首先注入一个ServerEndpointExporterBean,该Bean会自动注册使用@ServerEndpoint注解申明的websocket endpoint
      * @return
      */
-//    @Bean
-//    public ServerEndpointExporter serverEndpointExporter() {
-//        return new ServerEndpointExporter();
-//    }
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
 }
