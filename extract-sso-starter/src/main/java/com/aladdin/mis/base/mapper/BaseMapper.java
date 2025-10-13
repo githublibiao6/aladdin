943e091c11666373a83da01161917a80a4bfe429
lb <1446977718@qq.com>
2025-10-13 23:47:50 +0800

    粗糙的自定义实现

--- extract-sso-starter/src/main/java/com/aladdin/mis/base/mapper/BaseMapper.java
+++ extract-sso-starter/src/main/java/com/aladdin/mis/base/mapper/BaseMapper.java
@@ -6,23 +6,30 @@ import java.util.Collection;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
 public interface BaseMapper<T> extends Mapper<T> {
 
+    @Update("")
     int insert(T entity);
 
+    @Update("")
     int deleteById(Serializable id);
 
+    @Update("")
     int deleteById(T entity);
 
+    @Update("")
     int deleteBatchIds(@Param("coll") Collection<?> idList);
 
     @Update("")
     int updateById(@Param("et") T entity);
 
+    @Select("")
     T selectById(Serializable id);
 
+    @Select("")
     List<T> selectBatchIds(@Param("coll") Collection<? extends Serializable> idList);
 }
 
--- extract-sso-starter/src/main/java/com/aladdin/mis/base/mapper/PersonMapper.java
+++ extract-sso-starter/src/main/java/com/aladdin/mis/base/mapper/PersonMapper.java
@@ -7,8 +7,6 @@ import org.apache.ibatis.annotations.Update;
 @Mapper
 public interface PersonMapper extends BaseMapper<Person> {
 
-
     @Update("update person set name = #{name} where id #{id}")
     void updatePerson(Person person);
-
 }
